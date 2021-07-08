abcd6ac1eb72ce67fac1b31e89535b92dc482c23
AnArzhanykh <45142693+AnArzhanykh@users.noreply.github.com>
2021-07-08 12:16:04 +0300

    1

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,13 +1,13 @@
-import ref from './ref';
-import alert from './alert';
+// import ref from './ref';
+// import alert from './alert';
 
-export default function visibleBtn(arr){
-    if(arr.length < 12){
-        alert.imageFinished();
-        ref.BtnRef.classList.add('hidden');
-    };
-    if(arr.length >= 12){
-        ref.BtnRef.classList.remove('hidden');
-    }; 
-};
+// export default function visibleBtn(arr){
+//     if(arr.length < 12){
+//         alert.imageFinished();
+//         ref.BtnRef.classList.add('hidden');
+//     };
+//     if(arr.length >= 12){
+//         ref.BtnRef.classList.remove('hidden');
+//     }; 
+// };
 
